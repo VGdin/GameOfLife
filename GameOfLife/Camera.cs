ab6da0028b3d24e6488f77d541a2923b166b85f9
Valter Gådin <valter@gadin.nu>
2023-02-02 14:15:45 +0100

    Optimize translationMatrix only recalculate when necessary
    Change so that backing field is recalculated when dirty, i.e. when camera has moved/zoomed.

--- GameOfLife/Camera.cs
+++ GameOfLife/Camera.cs
@@ -14,13 +14,26 @@ namespace GameOfLife
     public sealed class Camera
     {
 
-        public Vector2 Position { get; private set; } = new Vector2(Config.Instance.GameResolution.widht/2, Config.Instance.GameResolution.height/2);
+        public Vector2 Position { get; private set; } = new Vector2(Config.Instance.GameResolution.widht / 2, Config.Instance.GameResolution.height / 2);
         public float Zoom { get; private set; } = Config.Instance.DefaultZoom;
         public int ViewPortWidth { get; set; } = Config.Instance.DefaultResolution.widht;
         public int ViewPortHeight { get; set; } = Config.Instance.DefaultResolution.height;
-        public Vector2 ViewPortCenter => new Vector2(ViewPortWidth /2, ViewPortHeight/2);
+        public Vector2 ViewPortCenter => new Vector2(ViewPortWidth / 2, ViewPortHeight / 2);
 
-        public Matrix TranslationMatrix => Matrix.CreateTranslation(- Position.X, -Position.Y,0) * Matrix.CreateScale(Zoom, Zoom,1) * Matrix.CreateTranslation(ViewPortCenter.X, ViewPortCenter.Y,0);
+        private bool _isDirty = true;
+        private Matrix _backingMatrix;
+        public Matrix TranslationMatrix
+        {
+            get
+            {
+                if (_isDirty)
+                {
+                    _isDirty = false;
+                    _backingMatrix =  Matrix.CreateTranslation(-Position.X, -Position.Y, 0) * Matrix.CreateScale(Zoom, Zoom, 1) * Matrix.CreateTranslation(ViewPortCenter.X, ViewPortCenter.Y, 0);
+                }
+                return _backingMatrix;
+            }
+        }
 
         public Camera()
         {
@@ -28,18 +41,20 @@ namespace GameOfLife
 
         public void ZoomCamera(ZoomActions action)
         {
+            _isDirty = true;
             Zoom = action switch
             {
                 ZoomActions.ZoomMin => Config.Instance.ZoomMin,
                 ZoomActions.ZoomMax => Config.Instance.ZoomMax,
                 ZoomActions.ZoomIn when Zoom < Config.Instance.ZoomMax => Zoom * 2,
-                ZoomActions.ZoomOut when Zoom > Config.Instance.ZoomMin => Zoom /2,
+                ZoomActions.ZoomOut when Zoom > Config.Instance.ZoomMin => Zoom / 2,
                 _ => Zoom
-            }; 
+            };
         }
 
         public void MoveCamera(CameraDirections direction)
         {
+            _isDirty = true;
             Position += direction switch
             {
                 CameraDirections.Up => new Vector2(0, -Config.Instance.CameraSpeed),
